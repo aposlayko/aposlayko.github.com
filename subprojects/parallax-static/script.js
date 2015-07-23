efe87c8ff72e75cce8d4ee6fbd170e2d9a26cbf3
maxkudla <maksim.kudla@gmail.com>
2015-07-23 17:20:49 +0300

    small changes in parallax-static subproject

---
+++ subprojects/parallax-static/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+$(function () {
+	$('body').mCustomScrollbar({
+		theme:"dark-thin",        
+        scrollInertia: 1000 
+	});
+});
--- subprojects/parallax/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-$(function () {
-	$('body').mCustomScrollbar({
-		theme:"dark-thin",
-        autoHideScrollbar: true,
-        alwaysShowScrollbar: 0	
-	});
-});
