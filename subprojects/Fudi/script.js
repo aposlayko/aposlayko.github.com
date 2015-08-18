fe706a7b417e3019f9a47a2433c0ecd1bd348a16
Alexandr Poslayko <aposlayko@gmail.com>
2015-08-18 10:25:41 +0300

    Fudi - add  one side animation

--- subprojects/Fudi/script.js
+++ subprojects/Fudi/script.js
@@ -1,80 +1,99 @@
 $(function() {
+	$('.item').hide();
+	$('.phone').hide();
+	$('.article').hide();
+	$('.slider').hide();
+	$('.food').hide();
+
+	function disableWaypoint (waypoint) {
+		setTimeout(function () { waypoint.disable(); }, 500);
+	}
+
+	var navDown = new Waypoint({
+	  element: document.getElementsByClassName('nav-menu'),	  
+
+	  handler: function(direction) {
+	  	var logo = $(this.element).find('.logo');
+	  	var menu = $(this.element).find('ul');
+	    if (direction === 'down') {
+	    	console.log('nav down');	    	
+	    	logo.addClass('animated bounceInLeft');
+	    	menu.addClass('animated bounceInRight');
+
+	    	logo.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {	    		
+                logo.removeClass('animated bounceInLeft');
+            });
+
+            menu.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {	    		
+                menu.removeClass('animated bounceInRight');
+            });
+
+            disableWaypoint(this);
+	    }    
+	  }
+	});	
+
   var w1 = new Waypoint({
-	  element: document.getElementById('s-1'),
-	  offset: '90%',
+	  element: document.getElementById('s-1'),	  
 	  handler: function(direction) {
 	  	var target = $(this.element).find('.center-wrap');
 	    if (direction === 'down') {
+	    	target.hide();
 	    	console.log('s-1 down');
-	    	
-	    	target.addClass('animated bounceInUp');
+	    	setTimeout(function () {
+	    		target.addClass('animated bounceInUp');
+	    		target.show();
+		    	target.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {	    		
+	                target.removeClass('animated bounceInUp');
+	            });
+	    	}, 500);
 
-	    	target.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {	    		
-                target.removeClass('animated bounceInUp');
-            });
-	    } else {
-
-	    }	    
+	    	disableWaypoint(this);	    	
+	    }    
 	  }
-	});
-
-  var w1u = new Waypoint({
-	  element: document.getElementById('s-1'),
-	  offset: '-90%',
-	  handler: function(direction) {
-	    if (direction === 'up') {
-	    	console.log('s-1 up');
-	    } else {
-
-	    }   
-	  }
-	});
+	});  
 
   var w2 = new Waypoint({
 	  element: document.getElementById('s-2'),
 	  offset: '80%',
 	  handler: function(direction) {
+	  	var targets = $(this.element).find('.item'),
+	  		time = 0;
+	  	
 	    if (direction === 'down') {
-	    	console.log('s-2 down');
-	    } else {
-
+	    	targets.each(function(index, item) {
+	    	time += 300;	    		
+	    		setTimeout(function () {
+	    			$(item).addClass('animated fadeInUp');
+	    			$(item).show();
+	    			$(item).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {	    		
+		                $(item).removeClass('animated fadeInUp');
+		            });		            
+	    		}, time);	    		
+	    	});
+	    	disableWaypoint(this);    		
 	    } 
 	  }
-	});
-
-  var w2u = new Waypoint({
-	  element: document.getElementById('s-2'),
-	  offset: '-50%',
-	  handler: function(direction) {
-	    if (direction === 'up') {
-	    	console.log('s-2 up');
-	    } else {
-
-	    } 
-	  }
-	});
+	});  
 
   var w3 = new Waypoint({
 	  element: document.getElementById('s-3'),
 	  offset: '70%',
 	  handler: function(direction) {
+	  	var phone = $(this.element).find('.phone'),
+	  		article = $(this.element).find('.article');
+
 	    if (direction === 'down') {
-	    	console.log('s-3 down');
-	    } else {
+	    	phone.addClass('animated slideInLeft');
+	    	article.addClass('animated slideInRight');
+	    	phone.add(article).show();	    	
+	    	phone.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {	    		
+		        phone.removeClass('animated slideInLeft');
+		        article.removeClass('animated slideInRight');		        
+		    });
 
-	    } 
-	  }
-	});
-
-  var w3u = new Waypoint({
-	  element: document.getElementById('s-3'),
-	  offset: '-120%',
-	  handler: function(direction) {
-	    if (direction === 'up') {
-	    	console.log('s-3 up');
-	    } else {
-
-	    }
+		    disableWaypoint(this);
+	    }	       
 	  }
 	});
 
@@ -82,34 +101,38 @@ $(function() {
 	  element: document.getElementById('s-4'),
 	  offset: '80%',
 	  handler: function(direction) {
+	  	var slider = $(this.element).find('.slider');
 	    if (direction === 'down') {
-	    	console.log('s-4 down');
-	    } else {
+	    	slider.addClass('animated lightSpeedIn');
+	    	slider.show();
+	    	slider.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {	    		
+		        slider.removeClass('animated lightSpeedIn');
+		    });
 
-	    } 
+		    disableWaypoint(this);
+	    }
 	  }
 	});
 
-  var w4u = new Waypoint({
-	  element: document.getElementById('s-4'),
-	  offset: '-50%',
-	  handler: function(direction) {
-	    if (direction === 'up') {
-	    	console.log('s-4 up');
-	    } else {
-
-	    } 
-	  }
-	});
-
+  
   var w5 = new Waypoint({
 	  element: document.getElementById('s-5'),
 	  offset: '90%',
 	  handler: function(direction) {
+	  	var food = $(this.element).find('.food'),
+	  		time = 300;
 	    if (direction === 'down') {
-	    	console.log('s-5 down');
-	    } else {
-	    	console.log('s-5 up');
+	    	food.each(function (index, item) {
+	    		time += 200;
+	    		setTimeout(function () {
+	    			$(item).addClass('animated flipInX');
+	    			$(item).show();
+	    			$(item).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {	    		
+				        $(item).removeClass('animated flipInX');
+				    });	    			
+	    		}, time);
+	    	});
+	    	disableWaypoint(this);
 	    } 
 	  }
 	});
